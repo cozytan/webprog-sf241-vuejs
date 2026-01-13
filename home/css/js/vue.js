835739d200fd6a9b0e9d1e853ab4741e15cbc7f0
Klein <nimkaim85@gmail.com>
2026-01-13 10:12:26 +0800

    Added Klein De Guzman Browser

--- home/css/js/vue.js
+++ home/css/js/vue.js
@@ -1,16 +1,29 @@
-  const app = Vue.createApp({
-   data() {
-    return {
-     Hahappy laNg: "Hahappy laNg"
-    }
-   }
-  })
-  
-  app.mount('#app')
+const { createApp } = Vue;
 
-  
+createApp({
+    data() {
+        return {
+            imageUrl: 'https://wallpapercave.com/wp/wp8253134.jpg', // Placeholder Sukuna image
+            imageAlt: 'Ryomen Sukuna',
+            description: 'The King of Curses'
+        }
+    },
+    template: `
+        <div class="chrome-window">
+            <div class="chrome-header">
+                <div class="tab">Sukuna - Fan Page</div>
+            </div>
 
+            <div class="address-bar-container">
+                <div class="address-bar">
+                    https://domain-expansion.com/sukuna
+                </div>
+            </div>
 
-
-
-
+            <div class="chrome-content">
+                <img :src="imageUrl" :alt="imageAlt" class="sukuna-img" />
+                <p class="caption">{{ description }}</p>
+            </div>
+        </div>
+    `
+}).mount('#app');
