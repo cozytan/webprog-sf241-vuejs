e196d6d2a28d894acfbc3e976402ba1ea632ea3b
Klein <nimkaim85@gmail.com>
2026-01-16 14:55:08 +0800

    import your personal profile website from midterms into vue.

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
+import personalprofile from './components/personalprofile.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('food-item', FoodItem2)
+app.component('personal-profile', personalprofile)
 app.mount('#app')
