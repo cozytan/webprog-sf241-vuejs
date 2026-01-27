68b0f31c293d3826e8c7ecf0b6059506699c349e
Klein <nimkaim85@gmail.com>
2026-01-27 14:56:59 +0800

    Changed main.js

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,14 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-// Fix: Remove "/components/" because the files are in the same folder as main.js
 import FoodItem from './FoodItem.vue' 
 import personalprofile from './personalprofile.vue'
 
 const app = createApp(App)
-
-// Register components globally so you can use them in any template
 app.component('food-item', FoodItem)
 app.component('personal-profile', personalprofile)
-
 app.mount('#app')
